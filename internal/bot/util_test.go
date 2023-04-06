ef509f7c5af7834d772c607666f21aa102ad0ef4
Victor Piolin <victor.piolin@gmail.com>
2023-04-05 23:04:58 -0400

    Add test with error on bot send

--- internal/bot/util_test.go
+++ internal/bot/util_test.go
@@ -1,9 +1,13 @@
 package bot
 
 import (
+	"bytes"
+	"errors"
+	"log"
 	"testing"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -55,4 +59,33 @@ func TestPostInConvWithReply(t *testing.T) {
 
 	// Assert that bot.Send was called with the correct argument
 	bot.AssertCalled(t, "Send", msg)
+}
+
+func TestPostInConvBotSendError(t *testing.T) {
+	// Create a new buffer to capture the log output
+	var buf bytes.Buffer
+
+	// Set the output of the logger to the buffer
+	log.SetOutput(&buf)
+
+	bot := new(MockBot)
+
+	msg := tgbotapi.NewMessage(123, "Hello, world!")
+	message := tgbotapi.Message{
+		Chat: &tgbotapi.Chat{
+			ID: 123,
+		},
+		MessageID: 456,
+	}
+
+	bot.On("Send", msg).Return(tgbotapi.Message{}, errors.New("STOOOOOP"))
+
+	// Call the function being tested
+	postInConv(bot, message, msg.Text, false)
+
+	// Assert that bot.Send was called with the correct argument
+	bot.AssertCalled(t, "Send", msg)
+
+	assert.Contains(t, buf.String(), "STOOOOOP")
+	assert.Contains(t, buf.String(), "Couldn't speak in the conversation")
 }

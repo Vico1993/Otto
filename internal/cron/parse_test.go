801e930b7c3b1d749d5a9531c799e8e16f837ea8
Victor Piolin <victor.piolin@gmail.com>
2023-06-01 17:45:50 +0200

    Try to add some test

--- internal/cron/parse_test.go
+++ internal/cron/parse_test.go
@@ -3,7 +3,10 @@ package cron
 import (
 	"testing"
 
+	"github.com/Vico1993/Otto/internal/database"
+	"github.com/Vico1993/Otto/internal/repository"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestCategoriesAndTagsMatch(t *testing.T) {
@@ -20,4 +23,20 @@ func TestCategoriesAndTagsWontMatch(t *testing.T) {
 
 	assert.Len(t, match, 0, "The list of tags and categories don't overlap, array should be empty")
 	assert.Equal(t, []string{}, match, "The list of tags and categories don't overlap, array should be empty")
+}
+
+type mockArticlRep struct {
+	mock.Mock
+}
+
+func (m mockArticlRep) Create(title string, published string, link string, source string, tags ...string) *database.Article {
+	return nil
+}
+
+func (m mockArticlRep) Find(key string, val string) *database.Article {
+	return nil
+}
+
+func TestFetchFeed(t *testing.T) {
+	repository.Article = new(mockArticlRep)
 }

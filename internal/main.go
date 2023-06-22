4e239b7c574fedfe28b28ad6a6e7d189d3e3daed
Victor Piolin <victor.piolin@gmail.com>
2023-06-22 18:04:06 -0400

    Add notification to know when an update is received

--- internal/main.go
+++ internal/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"os"
+
 	"github.com/subosito/gotenv"
 
 	"github.com/Vico1993/Otto/internal/cron"
 	"github.com/Vico1993/Otto/internal/database"
 	"github.com/Vico1993/Otto/internal/repository"
+	"github.com/Vico1993/Otto/internal/service"
 )
 
 func main() {
@@ -17,6 +20,11 @@ func main() {
 
 	// Load repository
 	repository.Init()
+
+	service.NewTelegramService().TelegramPostMessage(
+		os.Getenv("TELEGRAM_USER_CHAT_ID"),
+		`*Upgrade complete*! Ready to be even smarter and funnier than before. 🤖 🚀 ✨`,
+	)
 
 	// Initialisation of the cron
 	cron.Init()

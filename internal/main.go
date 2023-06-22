a387d7f751e7ae737f9db9dba175feb4a30d4d8d
Piolin Victor <victor.piolin@gmail.com>
2023-06-22 18:12:03 -0400

    Remove bot portion (#64)
    * Remove bot
    
    * Update readme and dep
    
    * [BOT] Bump version from v0.12.3 to v0.13.0
    
    * Add notification to know when an update is received
    
    ---------
    
    Co-authored-by: Vico1993 <Vico1993@users.noreply.github.com>

--- internal/main.go
+++ internal/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"os"
+
 	"github.com/subosito/gotenv"
 
-	"github.com/Vico1993/Otto/internal/bot"
 	"github.com/Vico1993/Otto/internal/cron"
 	"github.com/Vico1993/Otto/internal/database"
 	"github.com/Vico1993/Otto/internal/repository"
+	"github.com/Vico1993/Otto/internal/service"
 )
 
 func main() {
@@ -19,9 +21,11 @@ func main() {
 	// Load repository
 	repository.Init()
 
+	service.NewTelegramService().TelegramPostMessage(
+		os.Getenv("TELEGRAM_USER_CHAT_ID"),
+		`*Upgrade complete*! Ready to be even smarter and funnier than before. 🤖 🚀 ✨`,
+	)
+
 	// Initialisation of the cron
 	cron.Init()
-
-	// bot
-	bot.Init()
 }

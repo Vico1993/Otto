3dee56b6700aeab23e2faef99e61256c3988b9db
Victor Piolin <victor.piolin@gmail.com>
2023-07-06 11:26:04 -0400

    More logs

--- internal/cron/init.go
+++ internal/cron/init.go
@@ -66,6 +66,7 @@ func startJobForChat(chat *database.Chat) {
 		// Start at different time to avoid parsing all feed at the same time
 		when := getDelay(len(chat.Feeds)) * n
 
+		fmt.Println("Adding Job for " + chat.ChatId + " -> " + feed.Url)
 		_, err := scheduler.Every(1).
 			Hour().
 			Tag(chat.ChatId).
